a7886bcfa57d87cfbf99c5769bb19e9d584660d3
kosw <swko4512@gmail.com>
2023-10-30 17:24:17 +0900

    [FIX] 메인 디자인 수정

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,36 +1,46 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, Text, Image, StyleSheet, ScrollView, TouchableOpacity, Dimensions } from 'react-native';
 import { BarChart, XAxis } from 'react-native-svg-charts';
-const { width: SCREEN_WIDTH } = Dimensions.get("window");
-const labels = ['월', '화', '수', '목', '금', '토', '일'];
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { api_uri } from '@env';
+const { height: SCREEN_HEIGHT, width: SCREEN_WIDTH } = Dimensions.get("window");
 
 const Home = ({ onNavigateToMore, navigateToPhotoAnalysis, navigateToVideoAnalysis }) => {
 
   return (
-    <View Style={styles.container}>
-        <View style={styles.imageContainer}>
-          <Image
-            source={require('../../assets/default_profile.png')}
-            style={styles.image}
-          />
+    <View style={styles.container}>
+      <View style={styles.imageContainer1}>
+        <Image
+          source={require('../../assets/default_profile.png')}
+          style={styles.image}
+          resizeMode="contain"
+        />
+      </View>
+      
+      <View style={styles.middleImagesContainer}>
+        
       </View>
 
-      {/* <View style={styles.secondContainer}>
-        <TouchableOpacity onPress={navigateToPhotoAnalysis}>
-          <Image
-            source={require('../../assets/123.jpg')}
-            style={styles.rectangleFood}
-          />
-        </TouchableOpacity>
-        <TouchableOpacity onPress={navigateToVideoAnalysis}>
-          <Image
-            source={require('../../assets/321.jpg')}
-            style={styles.rectangleVideo}
-          />
-        </TouchableOpacity>
+      <View style={styles.bottomImagesContainer}>
+        <View style={styles.imageContainer2}>
+          <TouchableOpacity onPress={navigateToPhotoAnalysis}>
+            <Image
+              source={require('../../assets/cart.png')}
+              style={styles.image1}
+            />
+          </TouchableOpacity>
+        </View>
 
-      </View> */}
-    </View >
+        <View style={styles.imageContainer3}>
+          <TouchableOpacity onPress={navigateToVideoAnalysis}>
+            <Image
+              source={require('../../assets/infor.jpg')}
+              style={styles.image2}
+            />
+          </TouchableOpacity>
+        </View>
+      </View>
+    </View>
   );
 };
 
@@ -38,7 +48,40 @@ const styles = StyleSheet.create({
   container: {
     flexGrow: 1,
   },
-
+  imageContainer1: {
+    borderRadius: 50,
+    overflow: 'hidden',
+  },
+  bottomImagesContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    marginTop: SCREEN_HEIGHT * 0.024,
+  },
+  imageContainer2: {
+    overflow: 'hidden',
+  },
+  imageContainer3: {
+    overflow: 'hidden',
+  },
+  image: {
+    width: SCREEN_WIDTH * 0.2,
+    height: SCREEN_HEIGHT * 0.1,
+  },
+  image1: {
+    width: SCREEN_WIDTH * 0.2,
+    height: SCREEN_HEIGHT * 0.1,
+    borderRadius: 20,
+  },
+  image2: {
+    width: SCREEN_WIDTH * 0.2,
+    height: SCREEN_HEIGHT * 0.1,
+    borderRadius: 20,
+  },
+  middleImagesContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    marginTop: SCREEN_HEIGHT * 0.024,
+  },
 });
 
 export default Home;
