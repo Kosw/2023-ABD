66462c5ec08fc8750df0020090e7af2d832db2de
kosw <swko4512@gmail.com>
2023-10-31 22:22:58 +0900

    [ADD] 쓰레기 변수로 묶음

--- src/Photo/Photo_Analysis.js
+++ src/Photo/Photo_Analysis.js
@@ -3,27 +3,29 @@ import { View, Text, StyleSheet, Dimensions } from 'react-native';
 
 const { height: SCREEN_HEIGHT, width: SCREEN_WIDTH } = Dimensions.get("window");
 
+const trash = "페트병"; // 변수로 묶음
+
 const Photo_Analysis = ({ onNavigateToPhoto }) => {
   return (
     <View style={styles.container}>
       <Text style={styles.infoText}>업사이클링 종류와 방법</Text>
       <View style={styles.celi}>
-        <Text style={styles.cellText}>사진 속 쓰레기 : 페트병</Text>
+        <Text style={styles.cellText}>사진 속 쓰레기 : {trash}</Text>
       </View>
-      <Text style={styles.infoText}>페트병으로 만들 수 있는 업사이클링 제품들</Text>
+      <Text style={styles.infoText}>{trash}으로 만들 수 있는 업사이클링 제품들</Text>
       <View style={styles.column}>
         <View style={styles.cell}>
-          <Text style={styles.cellText}>페트병 화분</Text>
+          <Text style={styles.cellText}>{trash} 화분</Text>
           <Text style={styles.additionalText}>추가 내용 3</Text>
           <Text style={styles.additionalText}>추가 내용 4</Text>
         </View>
         <View style={styles.cell}>
-          <Text style={styles.cellText}>페트병 필통</Text>
+          <Text style={styles.cellText}>{trash} 필통</Text>
           <Text style={styles.additionalText}>추가 내용 5</Text>
           <Text style={styles.additionalText}>추가 내용 6</Text>
         </View>
         <View style={styles.cell}>
-          <Text style={styles.cellText}>페트병 필통</Text>
+          <Text style={styles.cellText}>{trash} 필통</Text>
           <Text style={styles.additionalText}>추가 내용 5</Text>
           <Text style={styles.additionalText}>추가 내용 6</Text>
         </View>
@@ -52,20 +54,20 @@ const styles = StyleSheet.create({
   celi: {
     width: SCREEN_WIDTH / 1.1,
     height: 100,
-    backgroundColor: '#EEEEEE', // 더 연한 회색으로 변경
+    backgroundColor: '#EEEEEE',
     justifyContent: 'center',
     alignItems: 'center',
     marginBottom: 10,
-    borderRadius: 10, // 각진 부분을 둥글게 만듦
+    borderRadius: 10,
   },
   cell: {
-    width: SCREEN_WIDTH / 1.5,
+    width: SCREEN_WIDTH / 1.2,
     height: 150,
-    backgroundColor: '#EEEEEE', // 더 연한 회색으로 변경
+    backgroundColor: '#EEEEEE',
     justifyContent: 'center',
     alignItems: 'center',
     marginBottom: 10,
-    borderRadius: 10, // 각진 부분을 둥글게 만듦
+    borderRadius: 10,
   },
   cellText: {
     fontSize: 20,
